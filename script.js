fa6e3caba1a2df34dac7462799b5151b1a539ac3
Dipak1121 <91940812+Dipak1121@users.noreply.github.com>
2023-12-05 21:39:14 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,31 @@
-function letterCombinations(input_digit) {
+function letterCombinations(digits) {
   //Complete the function
+	const digitToLetters = {
+        '2': 'abc',
+        '3': 'def',
+        '4': 'ghi',
+        '5': 'jkl',
+        '6': 'mno',
+        '7': 'pqrs',
+        '8': 'tuv',
+        '9': 'wxyz'
+    };
+	function generateCombinations(index, currentCombination, result) {
+        if (index === digits.length) {
+            result.push(currentCombination);
+            return;
+        }
+        const currentLetters = digitToLetters[digits[index]];
+        for (let i = 0; i < currentLetters.length; i++) {
+            generateCombinations(index + 1, currentCombination + currentLetters[i], result);
+        }
+    }
+	if (!digits || digits.length === 0) {
+        return [];
+    }
+    const result = [];
+    generateCombinations(0, '', result);
+    return result.sort();
 }
 
 module.exports = letterCombinations;
